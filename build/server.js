8cecc0384a1bf7a0b34416c214f561fefc66b830
leirdan <uouleirdan@gmail.com>
2022-11-10 20:09:43 -0300

    Incrementando a ferramenta "ts-node-dev "em substituição ao "nodemon"

--- build/server.js
+++ build/server.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("Started!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/start", (req, res, next) => {
+    res.status(200).send({ "best artist": "lana del rey" });
+});
+const PORT = 3001;
+app.listen(process.env.PORT || PORT, () => {
+    console.log("Server is open.");
+});
--- source/server.ts
+++ source/server.ts
@@ -1,1 +1,13 @@
-console.log("Started!");
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get("/start", (req: Request, res: Response, next:NextFunction) => {
+    res.status(200).send({"best artist": "lana del rey"});
+})
+
+const PORT = 3001
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log("Server is open.")
+})

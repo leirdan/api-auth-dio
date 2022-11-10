dd1f24367edba6d9720b9480a3a46b0a5cee53f6
leirdan <uouleirdan@gmail.com>
2022-11-10 20:43:34 -0300

    Adicionando rota para encontrar um único usuário

--- build/routes/users-routes.js
+++ build/routes/users-routes.js
@@ -5,17 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
-router.use("/users", (req, res, next) => {
-    const users = [
-        {
-            username: "alfred",
-            age: "64"
-        },
-        {
-            username: "robin",
-            age: "19"
-        }
-    ];
+const users = [
+    { username: "alfred", age: "64", uuid: "0" },
+    { username: "robin", age: "19", uuid: "1" }
+];
+router.get("/users", (req, res, next) => {
     res.status(200).json(users);
+});
+router.get("/users/:uuid", (req, res, next) => {
+    const uuid = req.params.uuid;
+    const requestedUser = users.find((user) => user.uuid == uuid);
+    res.status(200).json(requestedUser);
 });
 exports.default = router;
--- source/routes/users-routes.ts
+++ source/routes/users-routes.ts
@@ -2,18 +2,19 @@ import express, {Response, Request, NextFunction} from "express"
 
 const router = express.Router()
 
-router.use("/users", (req: Request, res: Response, next: NextFunction) => {
-    const users = [
-		{
-        username: "alfred",
-        age: "64"
-    	},
-		{
-        username: "robin",
-        age: "19"
-    	}
-	]
+const users = [
+	{ username: "alfred", age: "64", uuid: "0"},
+	{ username: "robin", age: "19", uuid: "1" }
+]
+
+router.get("/users", (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json(users)
+})
+
+router.get("/users/:uuid", (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+	const uuid = req.params.uuid
+	const requestedUser = users.find((user) => user.uuid == uuid)
+	res.status(200).json(requestedUser)
 })
 
 export default router

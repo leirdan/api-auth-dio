ae1302c800ea459ebb9823473b63476398120082
leirdan <uouleirdan@gmail.com>
2022-11-11 08:37:42 -0300

    Rota de atualizar usuário

--- build/routes/users-routes.js
+++ build/routes/users-routes.js
@@ -19,7 +19,15 @@ router.get("/users/:uuid", (req, res, next) => {
 });
 router.post("/users", (req, res, next) => {
     const newUser = req.body;
-    users.push(newUser);
+    users.push(req.body);
     res.status(201).json(newUser);
+});
+router.put("/users/:uuid", (req, res, next) => {
+    const uuid = req.params.uuid;
+    let userSelect = users.find((user) => user.uuid == uuid);
+    const alteredUser = req.body;
+    userSelect = alteredUser;
+    users.push(alteredUser);
+    res.status(200).json(userSelect);
 });
 exports.default = router;
--- source/routes/users-routes.ts
+++ source/routes/users-routes.ts
@@ -19,8 +19,17 @@ router.get("/users/:uuid", (req: Request<{uuid: string}>, res: Response, next: N
 
 router.post("/users", (req: Request, res: Response, next: NextFunction) => {
 	const newUser = req.body
-	users.push(newUser)
+	users.push(req.body)
 	res.status(201).json(newUser)
 })
 
+router.put("/users/:uuid", (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+	const uuid = req.params.uuid
+	let userSelect = users.find((user) => user.uuid == uuid)
+	const alteredUser = req.body
+	userSelect = alteredUser
+	users.push(alteredUser)
+	res.status(200).json(userSelect)
+	// TODO fix this, make the app render the normal users and altered users in insomnia
+})
 export default router
